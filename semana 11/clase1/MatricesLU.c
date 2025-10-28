824c23a20978cda1c98b54e6795e2c6d1d8e48ea
danmorahdez <danmorahdez@gmail.com>
2025-10-28 15:49:59 -0600

    Update MatricesLU.c

--- semana 11/clase1/MatricesLU.c
+++ semana 11/clase1/MatricesLU.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <math.h>
 
 int main(){
   
@@ -7,76 +6,101 @@ int main(){
   printf("Porfavor mencione el número de incógnitas\n");
   scanf("%d",&Orden);
 
-  printf("Porfavor digite los datos de la matriz\n");
+  printf("Porfavor digite los datos de la matriz");
 
-  // Cambiar la estructura para almacenar por filas
-  float Matriz[Orden][Orden+1];
+  float Matriz[Orden+1][Orden];
+  float L[Orden][Orden];
+  float U[Orden][Orden];
+
+  // Inicializar L y U
+  for(int i = 0; i < Orden; i++){
+    for(int j = 0; j < Orden; j++){
+      if(i == j)
+        L[i][j] = 1.0;
+      else
+        L[i][j] = 0.0;
+      U[i][j] = 0.0;
+    }
+  }
 
   for(int i = 0; i < Orden; i++){
     for (int j = 0; j < Orden + 1; j++){
-      printf("De el valor en la coordenada: [%d] [%d]: ",i,j);
-      scanf("%f", &Matriz[i][j]);
+      printf("De el valor en la coordenada: [%d] [%d]",j,i);
+      scanf("%f", &Matriz[j][i]);
     }
   }
 
   printf("Tu matriz es: \n");
   for(int i = 0; i < Orden; i++){
     for (int j = 0; j < Orden + 1; j++){
-      printf("%f \t",Matriz[i][j]);
+      printf("%f \t",Matriz[j][i]);
+    }
+    printf("\n");
+  }
+  
+  /*
+  int Orden = 4;
+  
+  float Matriz[5][4] = {
+    {2,1,3,1},{1,3,-1,1},{1,-1,2,-1},{1,2,-1,3},{10,12,8,14}
+  };
+  */
+
+//Factorización LU
+  for(int i = 0; i < Orden; i++){
+    // Calcular U
+    for(int j = i; j < Orden; j++){
+      float suma = 0;
+      for(int k = 0; k < i; k++){
+        suma += L[i][k] * U[k][j];
+      }
+      U[i][j] = Matriz[j][i] - suma;
+    }
+    
+    // Calcular L
+    for(int j = i+1; j < Orden; j++){
+      float suma = 0;
+      for(int k = 0; k < i; k++){
+        suma += L[j][k] * U[k][i];
+      }
+      L[j][i] = (Matriz[i][j] - suma) / U[i][i];
+    }
+  }
+
+  printf("\nMatriz L:\n");
+  for(int i = 0; i < Orden; i++){
+    for (int j = 0; j < Orden; j++){
+      printf("%f \t",L[i][j]);
     }
     printf("\n");
   }
 
-//Gauss-Seidel
-  float X[Orden];
-  float X_anterior[Orden];
-  int iteraciones = 100;
-  float tolerancia = 0.0001;
-  
-  //Valores iniciales
+  printf("\nMatriz U:\n");
   for(int i = 0; i < Orden; i++){
-    X[i] = 0;
-    X_anterior[i] = 0;
+    for (int j = 0; j < Orden; j++){
+      printf("%f \t",U[i][j]);
+    }
+    printf("\n");
   }
 
-  int convergio = 0;
-  for(int iter = 0; iter < iteraciones; iter++){
-    //Copiar valores anteriores
-    for(int i = 0; i < Orden; i++){
-      X_anterior[i] = X[i];
+//Resolver Ly = b
+  float Y[Orden];
+  for(int i = 0; i < Orden; i++){
+    float suma = 0;
+    for(int j = 0; j < i; j++){
+      suma += L[i][j] * Y[j];
     }
-    
-    //Calcular nuevos valores
-    for(int i = 0; i < Orden; i++){
-      float suma = 0;
-      for(int j = 0; j < Orden; j++){
-        if(j != i){
-          suma += Matriz[i][j] * X[j];
-        }
-      }
-      // Verificar que no haya división por cero
-      if(Matriz[i][i] == 0){
-        printf("Error: Elemento diagonal cero en fila %d\n", i+1);
-        return 1;
-      }
-      X[i] = (Matriz[i][Orden] - suma) / Matriz[i][i];
-    }
-    
-    //Verificar convergencia
-    float error = 0;
-    for(int i = 0; i < Orden; i++){
-      error += fabs(X[i] - X_anterior[i]);
-    }
-    
-    if(error < tolerancia){
-      printf("Convergio en %d iteraciones\n", iter+1);
-      convergio = 1;
-      break;
-    }
+    Y[i] = (Matriz[Orden][i] - suma) / L[i][i];
   }
 
-  if(!convergio){
-    printf("No convergio después de %d iteraciones\n", iteraciones);
+//Resolver Ux = y
+  float X[Orden];
+  for(int i = Orden-1; i >= 0; i--){
+    float suma = 0;
+    for(int j = i+1; j < Orden; j++){
+      suma += U[i][j] * X[j];
+    }
+    X[i] = (Y[i] - suma) / U[i][i];
   }
 
 // Mostrar resultados
@@ -85,24 +109,14 @@ int main(){
     printf("x%d = %f\n", i+1, X[i]);
   }
 
-//Verificar diagonal dominante
-  int dominante = 1;
+//Calcular la determinante
+  float mult = 1;
   for(int i = 0; i < Orden; i++){
-    float suma = 0;
-    for(int j = 0; j < Orden; j++){
-      if(j != i){
-        suma += fabs(Matriz[i][j]);
-      }
-    }
-    if(fabs(Matriz[i][i]) <= suma){
-      dominante = 0;
-      printf("Advertencia: La matriz no es diagonalmente dominante en fila %d\n", i+1);
-    }
+    mult = mult * U[i][i];
   }
-  
-  if(dominante){
-    printf("La matriz es diagonalmente dominante\n");
+  printf("El determinante es: %f \n",mult);
+  if(mult<1){
+    printf("Advertencia, el determinante tiene un valor muy bajo (<1)");
   }
 
-  return 0;
 }

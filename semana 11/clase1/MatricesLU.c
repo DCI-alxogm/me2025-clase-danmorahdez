576f7678c36702d97b5c3c48e7a7230c2e797b2c
danmorahdez <danmorahdez@gmail.com>
2025-10-29 12:57:35 -0600

    Update MatricesLU.c

--- semana 11/clase1/MatricesLU.c
+++ semana 11/clase1/MatricesLU.c
@@ -1,122 +1,313 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
-  
-  int Orden;
-  printf("Porfavor mencione el número de incógnitas\n");
-  scanf("%d",&Orden);
+int main() {
+    int Orden;
+    printf("Porfavor mencione el número de incógnitas\n");
+    scanf("%d", &Orden);
 
-  printf("Porfavor digite los datos de la matriz");
+    // Matriz de coeficientes y vector de valores separados
+    float Matriz[Orden][Orden];
+    float B[Orden];
+    float L[Orden][Orden];
+    float U[Orden][Orden];
 
-  float Matriz[Orden+1][Orden];
-  float L[Orden][Orden];
-  float U[Orden][Orden];
-
-  // Inicializar L y U
-  for(int i = 0; i < Orden; i++){
-    for(int j = 0; j < Orden; j++){
-      if(i == j)
-        L[i][j] = 1.0;
-      else
-        L[i][j] = 0.0;
-      U[i][j] = 0.0;
+    // Inicializar L y U
+    for (int i = 0; i < Orden; i++) {
+        for (int j = 0; j < Orden; j++) {
+            if (i == j)
+                L[i][j] = 1.0;
+            else
+                L[i][j] = 0.0;
+            U[i][j] = 0.0;
+        }
     }
-  }
 
-  for(int i = 0; i < Orden; i++){
-    for (int j = 0; j < Orden + 1; j++){
-      printf("De el valor en la coordenada: [%d] [%d]",j,i);
-      scanf("%f", &Matriz[j][i]);
+    // Ingresar matriz de coeficientes
+    printf("Porfavor digite los datos de la matriz de coeficientes:\n");
+    for (int i = 0; i < Orden; i++) {
+        for (int j = 0; j < Orden; j++) {
+            printf("De el valor en la coordenada: [%d][%d]: ", i, j);
+            scanf("%f", &Matriz[i][j]);
+        }
     }
-  }
 
-  printf("Tu matriz es: \n");
-  for(int i = 0; i < Orden; i++){
-    for (int j = 0; j < Orden + 1; j++){
-      printf("%f \t",Matriz[j][i]);
+    // Ingresar vector B
+    printf("Porfavor digite el vector de valores B:\n");
+    for (int i = 0; i < Orden; i++) {
+        printf("Valor b[%d]: ", i);
+        scanf("%f", &B[i]);
     }
-    printf("\n");
-  }
-  
-  /*
-  int Orden = 4;
-  
-  float Matriz[5][4] = {
-    {2,1,3,1},{1,3,-1,1},{1,-1,2,-1},{1,2,-1,3},{10,12,8,14}
-  };
-  */
 
-//Factorización LU
-  for(int i = 0; i < Orden; i++){
-    // Calcular U
-    for(int j = i; j < Orden; j++){
-      float suma = 0;
-      for(int k = 0; k < i; k++){
-        suma += L[i][k] * U[k][j];
-      }
-      U[i][j] = Matriz[j][i] - suma;
+    // Mostrar sistema
+    printf("\nTu sistema de ecuaciones es: \n");
+    for (int i = 0; i < Orden; i++) {
+        for (int j = 0; j < Orden; j++) {
+            printf("%.2f\t", Matriz[i][j]);
+        }
+        printf("| %.2f\n", B[i]);
     }
-    
-    // Calcular L
-    for(int j = i+1; j < Orden; j++){
-      float suma = 0;
-      for(int k = 0; k < i; k++){
-        suma += L[j][k] * U[k][i];
-      }
-      L[j][i] = (Matriz[i][j] - suma) / U[i][i];
+
+/*
+    // Factorización LU
+    for (int i = 0; i < Orden; i++) {
+        // Calcular U
+        for (int j = i; j < Orden; j++) {
+            float suma = 0;
+            U[i][j] = Matriz[i][j] - suma;
+        }
     }
-  }
+*/
 
-  printf("\nMatriz L:\n");
-  for(int i = 0; i < Orden; i++){
-    for (int j = 0; j < Orden; j++){
-      printf("%f \t",L[i][j]);
+/*
+    // Factorización LU
+    for (int i = 0; i < Orden; i++) {
+        // Calcular U
+        for (int j = i; j < Orden; j++) {
+            float suma = 0;
+            for (int k = 0; k < i; k++) {
+                suma += L[i][k] * U[k][j];
+            }
+            U[i][j] = Matriz[i][j] - suma;
+        }
     }
-    printf("\n");
-  }
+*/
 
-  printf("\nMatriz U:\n");
-  for(int i = 0; i < Orden; i++){
-    for (int j = 0; j < Orden; j++){
-      printf("%f \t",U[i][j]);
+/*
+    // Factorización LU
+    for (int i = 0; i < Orden; i++) {
+        // Calcular U
+        for (int j = i; j < Orden; j++) {
+            float suma = 0;
+            for (int k = 0; k < i; k++) {
+                suma += L[i][k] * U[k][j];
+            }
+            U[i][j] = Matriz[i][j] - suma;
+        }
+
+        // Calcular L
+        for (int j = i + 1; j < Orden; j++) {
+            float suma = 0;
+            L[j][i] = (Matriz[j][i] - suma) / U[i][i];
+        }
     }
-    printf("\n");
-  }
+*/
 
-//Resolver Ly = b
-  float Y[Orden];
-  for(int i = 0; i < Orden; i++){
-    float suma = 0;
-    for(int j = 0; j < i; j++){
-      suma += L[i][j] * Y[j];
+    // Factorización LU
+    for (int i = 0; i < Orden; i++) {
+        // Calcular U
+        for (int j = i; j < Orden; j++) {
+            float suma = 0;
+            for (int k = 0; k < i; k++) {
+                suma += L[i][k] * U[k][j];
+            }
+            U[i][j] = Matriz[i][j] - suma;
+        }
+
+        // Calcular L
+        for (int j = i + 1; j < Orden; j++) {
+            float suma = 0;
+            for (int k = 0; k < i; k++) {
+                suma += L[j][k] * U[k][i];
+            }
+            L[j][i] = (Matriz[j][i] - suma) / U[i][i];
+        }
     }
-    Y[i] = (Matriz[Orden][i] - suma) / L[i][i];
-  }
 
-//Resolver Ux = y
-  float X[Orden];
-  for(int i = Orden-1; i >= 0; i--){
-    float suma = 0;
-    for(int j = i+1; j < Orden; j++){
-      suma += U[i][j] * X[j];
+    printf("\nMatriz L:\n");
+    for (int i = 0; i < Orden; i++) {
+        for (int j = 0; j < Orden; j++) {
+            printf("%f \t", L[i][j]);
+        }
+        printf("\n");
     }
-    X[i] = (Y[i] - suma) / U[i][i];
-  }
 
-// Mostrar resultados
-  printf("\nLas soluciones son:\n");
-  for(int i = 0; i < Orden; i++){
-    printf("x%d = %f\n", i+1, X[i]);
-  }
+    printf("\nMatriz U:\n");
+    for (int i = 0; i < Orden; i++) {
+        for (int j = 0; j < Orden; j++) {
+            printf("%f \t", U[i][j]);
+        }
+        printf("\n");
+    }
 
-//Calcular la determinante
-  float mult = 1;
-  for(int i = 0; i < Orden; i++){
-    mult = mult * U[i][i];
-  }
-  printf("El determinante es: %f \n",mult);
-  if(mult<1){
-    printf("Advertencia, el determinante tiene un valor muy bajo (<1)");
-  }
+/*
+    // Guardar matrices L y U para reutilizar
+    float L_guardada[Orden][Orden];
+    float U_guardada[Orden][Orden];
+    for (int i = 0; i < Orden; i++) {
+        for (int j = 0; j < Orden; j++) {
+            L_guardada[i][j] = L[i][j];
+        }
+    }
+*/
+
+    // Guardar matrices L y U para reutilizar
+    float L_guardada[Orden][Orden];
+    float U_guardada[Orden][Orden];
+    for (int i = 0; i < Orden; i++) {
+        for (int j = 0; j < Orden; j++) {
+            L_guardada[i][j] = L[i][j];
+            U_guardada[i][j] = U[i][j];
+        }
+    }
+
+/*
+    // Función para resolver con cualquier vector B
+    void resolverSistema(float L[Orden][Orden], float U[Orden][Orden], float B[Orden]) {
+        // Resolver Ly = B
+        float Y[Orden];
+        for (int i = 0; i < Orden; i++) {
+            float suma = 0;
+            Y[i] = (B[i] - suma) / L[i][i];
+        }
+    }
+*/
+
+/*
+    // Función para resolver con cualquier vector B
+    void resolverSistema(float L[Orden][Orden], float U[Orden][Orden], float B[Orden]) {
+        // Resolver Ly = B
+        float Y[Orden];
+        for (int i = 0; i < Orden; i++) {
+            float suma = 0;
+            for (int j = 0; j < i; j++) {
+                suma += L[i][j] * Y[j];
+            }
+            Y[i] = (B[i] - suma) / L[i][i];
+        }
+    }
+*/
+
+/*
+    // Función para resolver con cualquier vector B
+    void resolverSistema(float L[Orden][Orden], float U[Orden][Orden], float B[Orden]) {
+        // Resolver Ly = B
+        float Y[Orden];
+        for (int i = 0; i < Orden; i++) {
+            float suma = 0;
+            for (int j = 0; j < i; j++) {
+                suma += L[i][j] * Y[j];
+            }
+            Y[i] = (B[i] - suma) / L[i][i];
+        }
+
+        // Resolver Ux = y
+        float X[Orden];
+        for (int i = Orden - 1; i >= 0; i--) {
+            float suma = 0;
+            X[i] = (Y[i] - suma) / U[i][i];
+        }
+    }
+*/
+
+    // Función para resolver con cualquier vector B
+    void resolverSistema(float L[Orden][Orden], float U[Orden][Orden], float B[Orden]) {
+        // Resolver Ly = B
+        float Y[Orden];
+        for (int i = 0; i < Orden; i++) {
+            float suma = 0;
+            for (int j = 0; j < i; j++) {
+                suma += L[i][j] * Y[j];
+            }
+            Y[i] = (B[i] - suma) / L[i][i];
+        }
+
+        // Resolver Ux = y
+        float X[Orden];
+        for (int i = Orden - 1; i >= 0; i--) {
+            float suma = 0;
+            for (int j = i + 1; j < Orden; j++) {
+                suma += U[i][j] * X[j];
+            }
+            X[i] = (Y[i] - suma) / U[i][i];
+        }
+
+        // Mostrar resultados
+        printf("\nLas soluciones son:\n");
+        for (int i = 0; i < Orden; i++) {
+            printf("x%d = %f\n", i + 1, X[i]);
+        }
+    }
+
+    // Resolver sistema inicial
+    printf("\n--- SOLUCION INICIAL ---");
+    resolverSistema(L_guardada, U_guardada, B);
+
+/*
+    // Calcular determinante
+    float mult = 1;
+    for (int i = 0; i < Orden; i++) {
+        mult = mult * U[i][i];
+    }
+*/
+
+    // Calcular determinante
+    float mult = 1;
+    for (int i = 0; i < Orden; i++) {
+        mult = mult * U[i][i];
+    }
+    printf("El determinante es: %f \n", mult);
+    if (mult < 1) {
+        printf("Advertencia, el determinante tiene un valor muy bajo (<1)\n");
+    }
+
+/*
+    // Bucle para cambiar vector B
+    char opcion;
+    do {
+        printf("\n¿Desea cambiar el vector B y resolver nuevamente? (s/n): ");
+        scanf(" %c", &opcion);
+    } while (opcion == 's' || opcion == 'S');
+*/
+
+/*
+    // Bucle para cambiar vector B
+    char opcion;
+    do {
+        printf("\n¿Desea cambiar el vector B y resolver nuevamente? (s/n): ");
+        scanf(" %c", &opcion);
+
+        if (opcion == 's' || opcion == 'S') {
+            printf("Ingrese el nuevo vector B:\n");
+            float nuevo_B[Orden];
+            for (int i = 0; i < Orden; i++) {
+                printf("Valor b[%d]: ", i);
+                scanf("%f", &nuevo_B[i]);
+            }
+        }
+    } while (opcion == 's' || opcion == 'S');
+*/
+
+    // Bucle para cambiar vector B
+    char opcion;
+    do {
+        printf("\n¿Desea cambiar el vector B y resolver nuevamente? (s/n): ");
+        scanf(" %c", &opcion);
+
+        if (opcion == 's' || opcion == 'S') {
+            printf("Ingrese el nuevo vector B:\n");
+            float nuevo_B[Orden];
+            for (int i = 0; i < Orden; i++) {
+                printf("Valor b[%d]: ", i);
+                scanf("%f", &nuevo_B[i]);
+            }
+
+            printf("\n--- SOLUCION CON NUEVO VECTOR B ---");
+            printf("\nNuevo sistema:\n");
+            for (int i = 0; i < Orden; i++) {
+                for (int j = 0; j < Orden; j++) {
+                    printf("%.2f\t", Matriz[i][j]);
+                }
+                printf("| %.2f\n", nuevo_B[i]);
+            }
+
+            // Reutilizar L y U guardadas
+            resolverSistema(L_guardada, U_guardada, nuevo_B);
+        }
+    } while (opcion == 's' || opcion == 'S');
+
+    return 0;
+}
 
 }
+
